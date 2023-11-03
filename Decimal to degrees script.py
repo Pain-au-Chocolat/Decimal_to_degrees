f437402dd6d5d36eed67aaa592723ed47457284a
Pain-au-Chocolat <80424665+Pain-au-Chocolat@users.noreply.github.com>
2023-11-03 21:52:20 +0100

    Update Decimal to degrees script.py

--- Decimal to degrees script.py
+++ Decimal to degrees script.py
@@ -1,76 +1,108 @@
 # Decimal to Degrees Converter
-# TODO: Make GUI compatible with functions
-# TODO: Make final angle... if its 700°, it shows 340°. or if its 700.50, it shows 340,50.
+
 import tkinter
 from tkinter import *
 import threading
 import time
 
-global decimal_switch
-
 def decimal_to_degrees():
-    user_input = (input("Input your angle in decimal form: "))
-    user_input = user_input.replace(",", ".")
-    separator = "."
-    if separator not in user_input:
-        user_input = user_input + ".0"
 
-    result_angle = user_input.split(separator, 1)[0]
+    try:
 
-    minutes_decimal_part = user_input.split(separator, 1)[-1]
-    minutes_decimal_part = ("0." + str(minutes_decimal_part))
-    minutes_decimal_part = float(minutes_decimal_part)
-    minutes = minutes_decimal_part * 60
-    result_minutes = str(minutes).split(separator, 1)[0]
+        global decimal_switch, result_minutes, result_seconds
+        decimal_switch = True
+        while True:
+            user_input_decimal = str(decimal_entry_box.get())
+            user_input_decimal = user_input_decimal.replace(",", ".")
+            separator = "."
+            if separator not in user_input_decimal:
+                user_input_decimal = user_input_decimal + ".0"
 
-    seconds_decimal_part = str(minutes).split(separator, 1)[-1]
-    seconds_decimal_part = ("0." + str(seconds_decimal_part))
-    seconds_decimal_part = float(seconds_decimal_part)
-    seconds = seconds_decimal_part * 60
-    result_seconds = str(seconds).split(separator, 1)[0]
+            result_angle = user_input_decimal.split(separator, 1)[0]
 
-    print(result_angle + "°" + result_minutes + "'" + result_seconds + '"')
+            try:
+                minutes_decimal_part = user_input_decimal.split(separator, 1)[-1]
+                minutes_decimal_part = ("0." + str(minutes_decimal_part))
+                minutes_decimal_part = float(minutes_decimal_part)
+                minutes = minutes_decimal_part * 60
+                result_minutes = str(minutes).split(separator, 1)[0]
 
+                seconds_decimal_part = str(minutes).split(separator, 1)[-1]
+                seconds_decimal_part = ("0." + str(seconds_decimal_part))
+                seconds_decimal_part = float(seconds_decimal_part)
+                seconds = seconds_decimal_part * 60
+                result_seconds = str(seconds).split(separator, 1)[0]
+            except ValueError:
+                decimal_entry_box.delete(0, tkinter.END)
+                degrees_entry_box.delete(0, tkinter.END)
+                minutes_entry_box.delete(0, tkinter.END)
+                seconds_entry_box.delete(0, tkinter.END)
+
+
+            if decimal_switch:
+                degrees_entry_box.delete(0, tkinter.END)
+                degrees_entry_box.insert(0, str(result_angle))
+                minutes_entry_box.delete(0, tkinter.END)
+                minutes_entry_box.insert(0, str(result_minutes))
+                seconds_entry_box.delete(0, tkinter.END)
+                seconds_entry_box.insert(0, str(result_seconds))
+            time.sleep(0.01)
+    except ValueError:
+        degrees_entry_box.delete(0, tkinter.END)
+        minutes_entry_box.delete(0, tkinter.END)
+        seconds_entry_box.delete(0, tkinter.END)
+        decimal_entry_box.delete(0, tkinter.END)
+
+e = threading.Thread(name='decimalToDegrees', target=decimal_to_degrees)
 def degrees_to_decimal():
 
-    while not decimal_switch:
+
+
+    global decimal_switch, result_decimal
+    while True:
         user_input_degrees = str(degrees_entry_box.get())
         user_input_minutes = str(minutes_entry_box.get())
         user_input_seconds = str(seconds_entry_box.get())
-        result_decimal = (float(user_input_degrees) if user_input_degrees != "" else 0.0) + \
-                         ((float(user_input_minutes)/60) if user_input_minutes != "" else 0.0) + \
-                         ((float(user_input_seconds)/3600) if user_input_seconds != "" else 0.0)
-        decimal_entry_box.delete(0, tkinter.END)
-        decimal_entry_box.insert(0, str(result_decimal))
-        time.sleep(0.1)
+        try:
+            result_decimal = (float(user_input_degrees) if user_input_degrees != "" else 0.0) + \
+                            ((float(user_input_minutes)/60) if user_input_minutes != "" else 0.0) + \
+                            ((float(user_input_seconds)/3600) if user_input_seconds != "" else 0.0)
+        except ValueError:
+            decimal_entry_box.delete(0, tkinter.END)
+            degrees_entry_box.delete(0, tkinter.END)
+            minutes_entry_box.delete(0, tkinter.END)
+            seconds_entry_box.delete(0, tkinter.END)
+
+        if not decimal_switch:
+            decimal_entry_box.delete(0, tkinter.END)
+            decimal_entry_box.insert(0, str(result_decimal))
+        time.sleep(0.01)
+
+
 
 
 t = threading.Thread(name='degreesToDecimal', target=degrees_to_decimal)
 
-
-
-window = Tk()
-window.title("Made for KMS <3")
-window.geometry("553x270")
-window.configure(bg='gray')
+global decimal_switch
 
 def window_focus():
     while True:
         global decimal_switch
         widget = window.focus_get()
-        print(widget)
         if str(widget) == ".!labelframe4.!entry":
             decimal_switch = True
-            print("decimal focused")
         else:
             decimal_switch = False
-            print("decimal NOT focused")
-        time.sleep(1)
-
-
-
+        time.sleep(0.01)
 
 f = threading.Thread(name='focus', target=window_focus)
+
+window = Tk()
+window.title("Vyrobené pre KMS <3")
+window.geometry("553x270")
+window.configure(bg='gray')
+
+
 
 frame1 = LabelFrame(window, bg="green", bd=5)
 frame1.grid(row=0, column=0)
@@ -136,7 +168,9 @@ decimal_entry_box.insert(0, "0")
 decimal_entry_box.grid(row=0, column=0)
 
 t.start()
+e.start()
 f.start()
+
 
 window.mainloop()
 
